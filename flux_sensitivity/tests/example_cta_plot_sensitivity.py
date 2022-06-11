6243053fb6cbc6f58f461e2eb01c4cefe35dfb31
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-06-11 16:59:37 +0200

    make the figures

---
+++ flux_sensitivity/tests/example_cta_plot_sensitivity.py
@@ -0,0 +1,322 @@
+#!/usr/bin/python
+import argparse
+import numpy as np
+import plenoirf
+import flux_sensitivity
+import os
+import sebastians_matplotlib_addons as seb
+import spectral_energy_distribution_units as sed
+from plenoirf.analysis import spectral_energy_distribution as sed_styles
+import json_numpy
+import binning_utils
+import cosmic_fluxes
+
+parser = argparse.ArgumentParser(
+    prog="plot_differential_sensitivity",
+    description=("Make plots"),
+)
+parser.add_argument(
+    "input_dir",
+    metavar="INPUT_DIR",
+    type=str,
+    help="Path of directory with the IRF and diff. sensitivity.",
+)
+args = parser.parse_args()
+
+
+AA = json_numpy.read_tree(args.input_dir)
+out_dir = os.path.join(args.input_dir, "plot")
+os.makedirs(out_dir, exist_ok=True)
+
+CONFIG = AA["config"]
+IRF = AA["irf"]
+
+energy_bin = binning_utils.Binning(bin_edges=IRF["energy_bin_edges_GeV"])
+x_lim_GeV = energy_bin["decade_limits"]
+y_lim_per_m2_per_s_per_GeV = np.array([1e3, 1e-16])
+
+observation_time = CONFIG["observation_time_s"]
+observation_time_str = "{:.0f}s".format(observation_time)
+internal_sed_style = sed_styles.PLENOIRF_SED_STYLE
+COSMIC_RAYS = {"total": "black"}
+
+# diff sens
+# ---------
+SED_STYLES = {
+    "portal": sed_styles.PLENOIRF_SED_STYLE,
+    "science": sed_styles.SCIENCE_SED_STYLE,
+    "fermi": sed_styles.FERMI_SED_STYLE,
+    "cta": sed_styles.CHERENKOV_TELESCOPE_ARRAY_SED_STYLE,
+}
+
+crab_flux = cosmic_fluxes.read_crab_nebula_flux_from_resources()
+fermi = plenoirf.other_instruments.fermi_lat
+cta = plenoirf.other_instruments.cherenkov_telescope_array_south
+
+
+# plot
+# ----
+for dk in flux_sensitivity.differential.SCENARIOS:
+    elabel = flux_sensitivity.differential.SCENARIOS[dk]["energy_axes_label"]
+    SCENARIO = AA["scenarios"][dk]
+
+    fig = seb.figure(plenoirf.summary.figure.FIGURE_STYLE)
+    ax = seb.add_axes(fig=fig, span=plenoirf.summary.figure.AX_SPAN)
+    for ck in COSMIC_RAYS:
+        ck_Rt = SCENARIO["background_rate_onregion_in_scenario_per_s"]
+        ck_Rt_au = SCENARIO["background_rate_onregion_in_scenario_per_s_au"]
+        seb.ax_add_histogram(
+            ax=ax,
+            bin_edges=energy_bin["edges"],
+            bincounts=ck_Rt,
+            linestyle="-",
+            linecolor=COSMIC_RAYS[ck],
+            linealpha=1.0,
+            bincounts_upper=ck_Rt + ck_Rt_au,
+            bincounts_lower=ck_Rt - ck_Rt_au,
+            face_color=COSMIC_RAYS[ck],
+            face_alpha=0.2,
+            label=None,
+            draw_bin_walls=False,
+        )
+    ax.set_ylabel("rate / s$^{-1}$")
+    ax.set_xlabel("reco. energy / GeV")
+    ax.set_ylim([1e-5, 1e5])
+    ax.loglog()
+    fig.savefig(
+        os.path.join(out_dir, "background_rate_in_scenario_{:s}.jpg".format(dk))
+    )
+    seb.close(fig)
+
+    fig = seb.figure(plenoirf.summary.figure.FIGURE_STYLE)
+    ax = seb.add_axes(fig=fig, span=plenoirf.summary.figure.AX_SPAN)
+    seb.ax_add_histogram(
+        ax=ax,
+        bin_edges=energy_bin["edges"],
+        bincounts=SCENARIO["signal_area_in_scenario_m2"],
+        linestyle="-",
+        linecolor="k",
+        linealpha=1.0,
+        bincounts_upper=SCENARIO["signal_area_in_scenario_m2"] + SCENARIO["signal_area_in_scenario_m2_au"],
+        bincounts_lower=SCENARIO["signal_area_in_scenario_m2"] - SCENARIO["signal_area_in_scenario_m2_au"],
+        face_color="k",
+        face_alpha=0.2,
+        label="Reco. energy, according to G-matrix for this scenario",
+        draw_bin_walls=False,
+    )
+    ax.plot(energy_bin["centers"], IRF["signal_area_m2"], "+k", label="True energy.")
+    ax.set_ylabel("area / m$^{2}$")
+    ax.set_xlabel(elabel + "energy / GeV")
+    ax.set_ylim([1e0, 1e7])
+    ax.loglog()
+    ax.legend(loc="best", fontsize=6)
+    fig.savefig(os.path.join(out_dir, "signal_area_in_scenario_{:s}.jpg".format(dk)))
+    seb.close(fig)
+
+    # G_matrix
+    # ---------------------------
+    G_matrix = SCENARIO["scenario"]["G_matrix"]
+    fig = seb.figure(seb.FIGURE_1_1)
+    ax_c = seb.add_axes(fig=fig, span=[0.16, 0.16, 0.7, 0.7])
+    ax_cb = seb.add_axes(fig=fig, span=[0.88, 0.16, 0.02, 0.7])
+    _pcm_confusion = ax_c.pcolormesh(
+        energy_bin["edges"],
+        energy_bin["edges"],
+        np.transpose(G_matrix),
+        cmap="Greys",
+        norm=seb.plt_colors.PowerNorm(gamma=0.5),
+        vmin=0,
+        vmax=1,
+    )
+    ax_c.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+    seb.plt.colorbar(_pcm_confusion, cax=ax_cb, extend="max")
+    ax_c.set_aspect("equal")
+    ax_c.set_ylabel("reco. energy / GeV")
+    ax_c.loglog()
+    ax_c.set_xlabel("energy / GeV")
+    fig.savefig(os.path.join(out_dir, "G_matrix_in_scenario_{:s}.jpg".format(dk)))
+    seb.close(fig)
+
+    # B_matrix
+    # --------
+    B_matrix = SCENARIO["scenario"]["B_matrix"]
+    fig = seb.figure(seb.FIGURE_1_1)
+    ax_c = seb.add_axes(fig=fig, span=[0.16, 0.16, 0.7, 0.7])
+    ax_cb = seb.add_axes(fig=fig, span=[0.88, 0.16, 0.02, 0.7])
+    _pcm_confusion = ax_c.pcolormesh(
+        energy_bin["edges"],
+        energy_bin["edges"],
+        np.transpose(B_matrix),
+        cmap="Greys",
+        norm=seb.plt_colors.PowerNorm(gamma=0.5),
+        vmin=0,
+        vmax=1,
+    )
+    ax_c.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+    seb.plt.colorbar(_pcm_confusion, cax=ax_cb, extend="max")
+    ax_c.set_aspect("equal")
+    ax_c.set_ylabel("reco. energy / GeV")
+    ax_c.loglog()
+    ax_c.set_xlabel("energy / GeV")
+    fig.savefig(os.path.join(out_dir, "B_matrix_in_scenario_{:s}.jpg".format(dk)))
+    seb.close(fig)
+
+
+
+
+
+    components = []
+
+    # Crab reference fluxes
+    # ---------------------
+    for i in range(4):
+        com = {}
+        scale_factor = np.power(10.0, (-1) * i)
+        com["energy"] = [np.array(crab_flux["energy"]["values"])]
+        com["differential_flux"] = [
+            scale_factor
+            * np.array(crab_flux["differential_flux"]["values"])
+        ]
+        com[
+            "label"
+        ] = None  # "{:1.1e} Crab".format(scale_factor) if i == 0 else None
+        com["color"] = "k"
+        com["alpha"] = 0.25 / (1.0 + i)
+        com["linestyle"] = "--"
+        components.append(com.copy())
+
+    # Fermi-LAT diff
+    # --------------
+    fermi_diff = fermi.differential_sensitivity(l=0, b=90)
+    com = {}
+    com["energy"] = [np.array(fermi_diff["energy"]["values"])]
+    com["differential_flux"] = [
+        np.array(fermi_diff["differential_flux"]["values"])
+    ]
+    com["label"] = fermi.LABEL + ", 10y, (l=0, b=90)"
+    com["color"] = fermi.COLOR
+    com["alpha"] = 1.0
+    com["linestyle"] = "-"
+    components.append(com)
+
+    # CTA South 1800s
+    # ---------------
+    cta_diff = cta.differential_sensitivity(
+        observation_time=observation_time
+    )
+    com = {}
+    com["energy"] = [np.array(cta_diff["energy"]["values"])]
+    com["differential_flux"] = [
+        np.array(cta_diff["differential_flux"]["values"])
+    ]
+    com["label"] = cta.LABEL + ", " + observation_time_str
+    com["color"] = cta.COLOR
+    com["alpha"] = 1.0
+    com["linestyle"] = "-"
+    components.append(com)
+
+    # My IRF
+    # ------
+    com = {}
+    com["energy"] = []
+    com["differential_flux"] = []
+    com["differential_flux_au"] = []
+
+    for ii in range(energy_bin["num"]):
+        com["energy"].append(
+            [energy_bin["edges"][ii], energy_bin["edges"][ii + 1]]
+        )
+        _dFdE_sens = SCENARIO["dVdE_per_m2_per_GeV_per_s"][ii]
+        com["differential_flux"].append([_dFdE_sens, _dFdE_sens])
+
+        _dFdE_sens_au = SCENARIO["dVdE_per_m2_per_GeV_per_s_au"][ii]
+        com["differential_flux_au"].append(
+            [_dFdE_sens_au, _dFdE_sens_au]
+        )
+
+    com["label"] = CONFIG["name"]
+    com["color"] = "black"
+    com["alpha"] = 1.0
+    com["linestyle"] = "-"
+    components.append(com)
+
+    for sedk in SED_STYLES:
+        sed_style = SED_STYLES[sedk]
+        sed_style_dirname = "sed_style_" + sedk
+        os.makedirs(os.path.join(out_dir, sed_style_dirname), exist_ok=True)
+
+
+        fig = seb.figure(plenoirf.summary.figure.FIGURE_STYLE)
+        ax = seb.add_axes(fig=fig, span=plenoirf.summary.figure.AX_SPAN)
+
+        for com in components:
+
+            for ii in range(len(com["energy"])):
+                _energy, _dFdE = sed.convert_units_with_style(
+                    x=com["energy"][ii],
+                    y=com["differential_flux"][ii],
+                    input_style=internal_sed_style,
+                    target_style=sed_style,
+                )
+
+                ax.plot(
+                    _energy,
+                    _dFdE,
+                    label=com["label"] if ii == 0 else None,
+                    color=com["color"],
+                    alpha=com["alpha"],
+                    linestyle=com["linestyle"],
+                )
+
+                if "differential_flux_au" in com:
+                    _, _dFdE_au = sed.convert_units_with_style(
+                        x=com["energy"][ii],
+                        y=com["differential_flux_au"][ii],
+                        input_style=internal_sed_style,
+                        target_style=sed_style,
+                    )
+                    _d_ru = _dFdE_au / _dFdE
+                    _dFdE_lu = _dFdE - _dFdE_au
+                    _dFdE_uu = _dFdE + _dFdE_au
+                    ax.fill_between(
+                        x=_energy,
+                        y1=_dFdE_lu,
+                        y2=_dFdE_uu,
+                        label=None,
+                        color=com["color"],
+                        alpha=com["alpha"] * 0.15,
+                        linewidth=0.0,
+                    )
+
+        _x_lim, _y_lim = sed.convert_units_with_style(
+            x=x_lim_GeV,
+            y=y_lim_per_m2_per_s_per_GeV,
+            input_style=internal_sed_style,
+            target_style=sed_style,
+        )
+
+        ax.set_xlim(np.sort(_x_lim))
+        ax.set_ylim(np.sort(_y_lim))
+        ax.loglog()
+        ax.legend(loc="best", fontsize=6)
+        etype = flux_sensitivity.differential.SCENARIOS[dk][
+            "energy_axes_label"
+        ]
+        ax.set_xlabel(
+            etype
+            + " "
+            + sed_style["x_label"]
+            + " /"
+            + sed_style["x_unit"]
+        )
+        ax.set_ylabel(
+            sed_style["y_label"] + " /\n " + sed_style["y_unit"]
+        )
+        fig.savefig(
+            os.path.join(
+                out_dir,
+                sed_style_dirname,
+                "differential_sensitivity_{:s}.jpg".format(dk),
+            )
+        )
+        seb.close(fig)
---
+++ flux_sensitivity/tests/example_cta_rebin_and_avergage_irf_and_estimate_sensitivity.py
@@ -0,0 +1,220 @@
+#!/usr/bin/python
+import argparse
+import flux_sensitivity as fs
+import binning_utils
+import os
+import json_numpy
+import numpy as np
+
+
+parser = argparse.ArgumentParser(
+    prog="estimate_differential_sensitivity",
+    description=("Reads an instrument's response function from a gamma-astro-data-fits-file and estimates the instrument's differential sensitivity"),
+)
+
+parser.add_argument(
+    "irf_file",
+    metavar="IRF_PATH",
+    type=str,
+    help="gamma-astro-data-fits-file containing the instrument's response function.",
+)
+parser.add_argument(
+    "out_dir",
+    metavar="OUT_DIR",
+    type=str,
+    help="writes the output here.",
+)
+
+parser.add_argument(
+    "--num_bins_per_decade",
+    metavar="NUM",
+    type=int,
+    help="Number of bins per decade",
+    default=5,
+)
+parser.add_argument(
+    "--roi_opening_deg",
+    metavar="ANGLE",
+    type=float,
+    help="Opening angle of region-of-interest in field-of-view.",
+    default=3.25,
+)
+parser.add_argument(
+    "--detection_threshold_std",
+    metavar="STDDEV",
+    type=float,
+    help="Standard-deviation a signal has to reach above the background's noise.",
+    default=5.0,
+)
+parser.add_argument(
+    "--systematic_uncertainty_relative",
+    metavar="SYS",
+    type=float,
+    help="Relative systematic uncertainty of the instrument.",
+    default=1e-2,
+)
+parser.add_argument(
+    "--observation_time_s",
+    metavar="TOBS",
+    type=float,
+    help="The observation-time in s.",
+    default=1800,
+)
+parser.add_argument(
+    "--on_over_off_ratio",
+    metavar="ALPHA",
+    type=float,
+    help="Ratio of on-region over off-region.",
+    default=0.2,
+)
+parser.add_argument(
+    "--estimator_statistics",
+    metavar="METHOD",
+    type=str,
+    help="The name of the estimator for the statistics. ['sqrt', 'LiMaEq9', 'LiMaEq17']",
+    default="LiMaEq17",
+)
+
+
+args = parser.parse_args()
+irf_path = args.irf_file
+IRF_NAME = os.path.basename(irf_path)
+out_dir = args.out_dir
+
+CONFIG = {}
+CONFIG["name"] = IRF_NAME
+CONFIG["num_bins_per_decade"] = args.num_bins_per_decade
+CONFIG["roi_opening_deg"] = args.roi_opening_deg
+CONFIG["detection_threshold_std"] = args.detection_threshold_std
+CONFIG["estimator_statistics"] = args.estimator_statistics
+CONFIG["systematic_uncertainty_relative"] = args.systematic_uncertainty_relative
+CONFIG["observation_time_s"] = args.observation_time_s
+CONFIG["on_over_off_ratio"] = args.on_over_off_ratio
+
+
+irf = fs.io.gamma_astro_data.read_instrument_response_function(
+    path=irf_path
+)
+
+irf = fs.io.gamma_astro_data.average_instrument_response_over_field_of_view(
+    irf=irf, roi_opening_deg=CONFIG["roi_opening_deg"]
+)
+
+energy_bin_edges_GeV = fs.io.gamma_astro_data.find_common_energy_bin_edges(
+    components=irf, num_bins_per_decade=CONFIG["num_bins_per_decade"],
+)
+
+probability_reco_given_true = fs.io.gamma_astro_data.integrate_dPdMu_to_get_probability_reco_given_true(
+    dPdMu=irf["energy_dispersion"]["dPdMu"],
+    dPdMu_energy_bin_edges=irf["energy_dispersion"]["energy_bin_edges_GeV"],
+    dPdMu_Mu_bin_edges=irf["energy_dispersion"]["Mu_bin_edges"],
+    energy_bin_edges=energy_bin_edges_GeV,
+)
+probability_reco_given_true_au = np.zeros(probability_reco_given_true.shape)
+
+signal_area_m2 = np.interp(
+    x=binning_utils.centers(energy_bin_edges_GeV),
+    xp=binning_utils.centers(irf["effective_area"]["energy_bin_edges_GeV"]),
+    fp=irf["effective_area"]["area_m2"],
+)
+signal_area_m2_au = np.zeros(signal_area_m2.shape)
+
+background_per_s_per_sr_per_GeV = np.interp(
+    x=binning_utils.centers(energy_bin_edges_GeV),
+    xp=binning_utils.centers(irf["background"]["energy_bin_edges_GeV"]),
+    fp=irf["background"]["background_per_s_per_sr_per_GeV"],
+)
+
+point_spread_function_sigma_deg = np.interp(
+    x=binning_utils.centers(energy_bin_edges_GeV),
+    xp=binning_utils.centers(
+        irf["point_spread_function"]["energy_bin_edges_GeV"]
+    ),
+    fp=irf["point_spread_function"]["sigma_deg"],
+)
+
+background_rate_onregion_per_s = fs.io.gamma_astro_data.integrate_background_rate_in_onregion(
+    background_per_s_per_sr_per_GeV=background_per_s_per_sr_per_GeV,
+    point_spread_function_sigma_deg=point_spread_function_sigma_deg,
+    energy_bin_edges_GeV=energy_bin_edges_GeV,
+)
+background_rate_onregion_per_s_au = np.zeros(background_rate_onregion_per_s.shape)
+
+
+os.makedirs(out_dir, exist_ok=True)
+blk = {}
+blk["energy_bin_edges_GeV"] = energy_bin_edges_GeV
+blk["probability_reco_given_true"] = probability_reco_given_true
+blk["probability_reco_given_true_au"] = probability_reco_given_true_au
+blk["signal_area_m2"] = signal_area_m2
+blk["signal_area_m2_au"] = signal_area_m2_au
+blk["background_rate_onregion_per_s"] = background_rate_onregion_per_s
+blk["background_rate_onregion_per_s_au"] = background_rate_onregion_per_s_au
+json_numpy.write(os.path.join(out_dir, "irf.json"), blk)
+json_numpy.write(os.path.join(out_dir, "config.json"), CONFIG)
+
+scenario_dir = os.path.join(out_dir, "scenarios")
+os.makedirs(scenario_dir, exist_ok=True)
+
+for scenario_key in fs.differential.SCENARIOS:
+    scenario = fs.differential.init_scenario_matrices_for_signal_and_background(
+        probability_reco_given_true=probability_reco_given_true,
+        probability_reco_given_true_au=probability_reco_given_true_au,
+        scenario_key=scenario_key,
+    )
+
+    (
+        signal_area_in_scenario_m2,
+        signal_area_in_scenario_m2_au,
+    ) = fs.differential.apply_scenario_to_signal_effective_area(
+        signal_area_m2=signal_area_m2,
+        signal_area_m2_au=signal_area_m2_au,
+        scenario_G_matrix=scenario["G_matrix"],
+        scenario_G_matrix_au=scenario["G_matrix_au"],
+    )
+
+    (
+        background_rate_onregion_in_scenario_per_s,
+        background_rate_onregion_in_scenario_per_s_au,
+    ) = fs.differential.apply_scenario_to_background_rate(
+        rate_in_reco_energy_per_s=background_rate_onregion_per_s,
+        rate_in_reco_energy_per_s_au=background_rate_onregion_per_s_au,
+        scenario_B_matrix=scenario["B_matrix"],
+        scenario_B_matrix_au=scenario["B_matrix_au"],
+    )
+
+    (
+        critical_signal_rate_in_scenario_per_s,
+        critical_signal_rate_in_scenario_per_s_au,
+    ) = fs.differential.estimate_critical_signal_rate_vs_energy(
+        background_rate_onregion_in_scenario_per_s=background_rate_onregion_in_scenario_per_s,
+        background_rate_onregion_in_scenario_per_s_au=background_rate_onregion_in_scenario_per_s_au,
+        onregion_over_offregion_ratio=CONFIG["on_over_off_ratio"],
+        observation_time_s=CONFIG["observation_time_s"],
+        instrument_systematic_uncertainty_relative=CONFIG["systematic_uncertainty_relative"],
+        detection_threshold_std=CONFIG["detection_threshold_std"],
+        estimator_statistics=CONFIG["estimator_statistics"],
+    )
+
+    (dVdE_per_m2_per_GeV_per_s, dVdE_per_m2_per_GeV_per_s_au,) = fs.differential.estimate_differential_sensitivity(
+        energy_bin_edges_GeV=energy_bin_edges_GeV,
+        signal_area_in_scenario_m2=signal_area_in_scenario_m2,
+        signal_area_in_scenario_m2_au=signal_area_in_scenario_m2_au,
+        critical_signal_rate_in_scenario_per_s=critical_signal_rate_in_scenario_per_s,
+        critical_signal_rate_in_scenario_per_s_au=critical_signal_rate_in_scenario_per_s_au,
+    )
+
+    out = {}
+    out["scenario"] = scenario
+
+    out["signal_area_in_scenario_m2"] = signal_area_in_scenario_m2
+    out["signal_area_in_scenario_m2_au"] = signal_area_in_scenario_m2_au
+
+    out["background_rate_onregion_in_scenario_per_s"] = background_rate_onregion_in_scenario_per_s
+    out["background_rate_onregion_in_scenario_per_s_au"] = background_rate_onregion_in_scenario_per_s_au
+
+    out["dVdE_per_m2_per_GeV_per_s"] = dVdE_per_m2_per_GeV_per_s
+    out["dVdE_per_m2_per_GeV_per_s_au"] = dVdE_per_m2_per_GeV_per_s_au
+
+    path = os.path.join(scenario_dir, "{:s}.json".format(scenario_key))
+    json_numpy.write(path, out)
