9a7f7843d8724b0629445754f1964c41ea66c8b0
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-08-07 09:22:04 +0200

    rename scenarios

--- flux_sensitivity/tests/test_scenario_black.py
+++ flux_sensitivity/tests/test_scenario_black.py
@@ -2,12 +2,12 @@ import flux_sensitivity
 import numpy as np
 
 
-def test_bell_spectrum_mask_shape():
+def test_scenario_black_mask_shape():
 
     for n in range(3, 19):
         Prgt = np.eye(n)
 
-        mask = flux_sensitivity.differential.scenarios.bell_spectrum.init_B(
+        mask = flux_sensitivity.differential.scenarios.black.init_matrix_B(
             probability_reco_given_true=Prgt, containment=0.68,
         )
 
